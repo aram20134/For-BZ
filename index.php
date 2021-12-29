6ad94d66c0670dbf3a6228dbd258dec1e724a5b8
aram20134 <dasigty@gmail.com>
2021-12-29 19:56:45 +0300

    +

--- index.php
+++ index.php
@@ -6,25 +6,23 @@ require "db.php";
 <?php
     $serverid = "7448";
 
-$url="https://api.trackyserver.com/widget/index.php?id=".$server_id;
+$url="https://api.trackyserver.com/widget/index.php?id=7448";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL,$url);
 $result=json_decode(curl_exec($ch), true);
 curl_close($ch);
  ?>
-    <div class="online-log">
-        <div class="box-online first map-anaxes">
-            <div class="text-ivent">
-                
-            </div>
-            <div class="text-online">
-                <?php echo $result['playerscount']; ?>
-            </div>
-        </div>  
-        <div class="box-online second map-corelia">
-        </div>
-    </div>
+ <?php
+	$serverid = "660455";
+
+$url2="https://api.trackyserver.com/widget/index.php?id=660455";
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch2, CURLOPT_URL,$url2);
+$result2=json_decode(curl_exec($ch2), true);
+curl_close($ch2);
+ ?>
     <div class="content">
         <div class="head-logo">
             
@@ -38,6 +36,19 @@ curl_close($ch);
                 <br><br>
                 Если у вас есть вопросы, предложения или вы нашли баг, обратитесь к Fish'у #3493.
             </p>
+        </div>
+        <h1 class="online-head">СТАТУС СЕРВЕРОВ</h1>
+        <div class="online-log">
+            <div class="box-online first map-anaxes">
+                <div class="text-online">
+                	<?php echo $result['playerscount']; ?>
+                </div>
+            </div>  
+            <div class="box-online second map-corelia">
+            	<div class="text-online">
+                	<?php echo $result2['playerscount']; ?>
+                </div>
+            </div>
         </div>
     </div>
 <?php 
